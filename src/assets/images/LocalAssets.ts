4698322f06cddc7964e8ae35ed9985c6312ae6d7
Aldrin <edwardaldrincastro@gmail.com>
2024-10-11 01:31:45 +0800

    updated images import to require for dynamic import

--- src/assets/images/LocalAssets.ts
+++ src/assets/images/LocalAssets.ts
@@ -1,17 +1,10 @@
-import illustrationComingSoon from "./illustration_coming_soon/illustration_coming_soon.png";
-import illustrationFaq1 from "./illustration_faq_1/illustration_faq_1.png";
-import illustrationFaq2 from "./illustration_faq_2/illustration_faq_2.png";
-import illustrationFaq3 from "./illustration_faq_3/illustration_faq_3.png";
-import illustrationNoInternet from "./illustration_no_internet/illustration_no_internet.png";
-import illustrationOnboarding from "./illustration_onboarding/illustration_onboarding_1.png";
-
 export const LocalAssets = {
   illustration: {
-    comingSoon: illustrationComingSoon,
-    faq1: illustrationFaq1,
-    faq2: illustrationFaq2,
-    faq3: illustrationFaq3,
-    noInternet: illustrationNoInternet,
-    onboarding: illustrationOnboarding,
+    comingSoon: require("./illustration_coming_soon/illustration_coming_soon.png"),
+    faq1: require("./illustration_faq_1/illustration_faq_1.png"),
+    faq2: require("./illustration_faq_2/illustration_faq_2.png"),
+    faq3: require("./illustration_faq_3/illustration_faq_3.png"),
+    noInternet: require("./illustration_no_internet/illustration_no_internet.png"),
+    onboarding: require("./illustration_onboarding/illustration_onboarding_1.png"),
   },
 };
