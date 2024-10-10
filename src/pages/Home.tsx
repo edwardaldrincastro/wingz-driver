61a3b59e1f13c9020256d20e1ff406399eb62c48
Aldrin <edwardaldrincastro@gmail.com>
2024-10-11 01:32:09 +0800

    updated home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,19 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, View } from "react-native";
 
-import { centerHV, flexChild } from "../styles";
+import { IcoMoon } from "../icons";
+import { centerHV, colorBlue, colorGreen, flexChild, fs16BoldBlack2, fs16MedBlack1, sh32 } from "../styles";
 
 export const Home = () => {
   const navigation = useNavigation();
 
   return (
     <View style={{ ...flexChild, ...centerHV }}>
-      <Text>Home Page</Text>
-      <Text onPress={() => navigation.navigate("Details")}>Go to Details</Text>
+      <IcoMoon size={sh32} name="arrow-left" color={colorGreen._0} />
+      <Text style={{ ...fs16BoldBlack2, color: colorGreen._0, fontSize: 24 }}>Home Page</Text>
+      <Text style={{ ...fs16MedBlack1, color: colorBlue._0 }} onPress={() => navigation.navigate("Details")}>
+        Go to Details
+      </Text>
     </View>
   );
 };
