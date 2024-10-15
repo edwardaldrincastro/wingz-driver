7a008123c83976634aded3b4ece254fa51077b11
Aldrin <edwardaldrincastro@gmail.com>
2024-10-16 04:04:24 +0800

    updated english

--- src/constants/language/english.ts
+++ src/constants/language/english.ts
@@ -11,6 +11,19 @@ export const english = {
       LABEL_ONGOING: "Ongoing Trips",
       LABEL_RECENT: "Recent",
     },
+    AUTH: {
+      BUTTON_LOGIN: "Log In",
+      BUTTON_SIGN_UP: "Sign Up",
+      HEADING_1: "Elevate Your Earnings with Airport Rides!",
+      SUBHEADING_1:
+        "Maximize your income by accepting requests that ensure higher fares, allowing you to know exactly what to expect before you hit the road.",
+      HEADING_2: "Make an Impact with NEMT Rides!",
+      SUBHEADING_2:
+        "Serve your community while building meaningful relationships, as you help those in need and truly make a difference in their lives.",
+      HEADING_3: "Boost Your Earnings with Event Rides!",
+      SUBHEADING_3:
+        "Enjoy the benefits of longer rides and increased tips from favorite driver requests, making each trip both rewarding and enjoyable.",
+    },
     HOME: {
       BUTTON_END: "End Trip",
       BUTTON_LATER: "Later",
