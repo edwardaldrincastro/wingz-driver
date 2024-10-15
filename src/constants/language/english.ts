6e2a0988dace5c888024e8b8981ca31da46110ba
Aldrin <edwardaldrincastro@gmail.com>
2024-10-16 01:07:01 +0800

    updated english and date constants, added legacy map style

--- src/constants/language/english.ts
+++ src/constants/language/english.ts
@@ -1,13 +1,25 @@
 export const english = {
-  PAGE: {
+  COMPONENTS: {
     ACTION_BUTTONS: {
-      BUTTON_CANCEL: "Cancel",
-      BUTTON_CONTINUE: "Continue",
+      BUTTON_CANCEL: "Decline",
+      BUTTON_CONTINUE: "Accept",
     },
-    TOAST: {
-      LABEL_CREDENTIALS_DELETED: "Credential(s) Deleted",
-      LABEL_DEFAULT: "Updated",
-      LABEL_UNDO: "Undo",
+  },
+  PAGE: {
+    ACTIVITY: {
+      LABEL_ACTIVITY: "Activity",
+      LABEL_ONGOING: "Ongoing Trips",
+      LABEL_RECENT: "Recent",
+    },
+    HOME: {
+      BUTTON_END: "End Trip",
+      BUTTON_LATER: "Later",
+      BUTTON_TRY: "Try Again",
+      BUTTON_YES: "Yes",
+      LABEL_DESTINATION: "Destination",
+      LABEL_PICKUP: "Pick Up Location",
+      LABEL_STATUS: "Status",
+      LABEL_WHEN: "When",
     },
   },
 };
